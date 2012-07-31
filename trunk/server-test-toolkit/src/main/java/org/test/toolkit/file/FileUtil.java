48561ebe78f1808a12ca385afdaae27ff8f8e44c
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2012-07-31 01:23:54 +0000

    add getRandomBytes method

--- trunk/server-test-toolkit/src/main/java/org/test/toolkit/file/FileUtil.java
+++ trunk/server-test-toolkit/src/main/java/org/test/toolkit/file/FileUtil.java
@@ -8,40 +8,42 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.UUID;
 
+import org.apache.commons.io.IOUtils;
 import org.test.toolkit.util.DigestUtil;
 import org.test.toolkit.util.DigestUtil.DigestAlgorithm;
+import org.test.toolkit.util.ValidationUtil;
 
 public final class FileUtil {
 
-	private FileUtil(){
+	private FileUtil() {
 
 	}
 
 	static String calcuteSHA_1(byte[] byteArray) {
- 		return DigestUtil.getDigest(byteArray, DigestAlgorithm.SHA_1);
- 	}
+		return DigestUtil.getDigest(byteArray, DigestAlgorithm.SHA_1);
+	}
 
 	static String calcuteSHA_1(InputStream inputStream) throws IOException {
- 		return DigestUtil.getDigest(inputStream, DigestAlgorithm.SHA_1);
+		return DigestUtil.getDigest(inputStream, DigestAlgorithm.SHA_1);
 
 	}
 
- 	static byte[] generateBytesBySize(long sizeByByteUnit) {
-		long targetFileSize = sizeByByteUnit;
+	static byte[] generateBytesBySize(long sizeByByteUnit) {
+		ValidationUtil.effectivePositive(sizeByByteUnit);
+
 		int allocateSize = 32;
 
 		ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
-		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(
-				byteOutputStream, 1024 * 512);
+		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(byteOutputStream, 1024 * 512);
 
 		int storeSize = 0;
 		byte[] bytes = null;
 		try {
-			while (storeSize < targetFileSize) {
+			while (storeSize < sizeByByteUnit) {
 				byte[] array = new byte[allocateSize];
 				Arrays.fill(array, (byte) (new Random().nextInt()));
 
-				int needBytesCount = (int) (targetFileSize - storeSize);
+				int needBytesCount = (int) (sizeByByteUnit - storeSize);
 				if (allocateSize > needBytesCount)
 					bufferedOutputStream.write(array, 0, needBytesCount);
 				else
@@ -54,20 +56,19 @@ public final class FileUtil {
 		} finally {
 			byte[] byteArray = byteOutputStream.toByteArray();
 			bytes = byteArray;
-			if (bufferedOutputStream != null)
-				try {
-					bufferedOutputStream.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+			IOUtils.closeQuietly(bufferedOutputStream);
 
 		}
 
 		return bytes;
 	}
 
-	static String newRandomStringContent() {
+	static String getRandomStringContent() {
 		return UUID.randomUUID().toString();
 	}
- 
+
+	static int getRandomBytes() {
+		return new Random().nextInt(10000) + 1;
+	}
+
 }
