2dc3dddf168558d9db3d0a0c830e4a7b84d9acad
fujian1115 <fujian1115@gmail.com>
2012-07-31 01:19:42 +0000

    change to filetype

--- server-test-toolkit/src/main/java/org/test/toolkit/file/JpgFile.java
+++ server-test-toolkit/src/main/java/org/test/toolkit/file/JpgFile.java
@@ -7,6 +7,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Random;
 
+import org.apache.commons.io.IOUtils;
+
 import com.sun.image.codec.jpeg.ImageFormatException;
 import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGImageEncoder;
@@ -14,29 +16,29 @@ import com.sun.image.codec.jpeg.JPEGImageEncoder;
 @SuppressWarnings("restriction")
 public class JpgFile extends RandomFile {
 
-	public static final String EXTENSION = ".jpg";
+	public static final String EXTENSION = FileType.Image.JPG.toString();
+
+	public JpgFile() {
+		this(getRandomWidthOrHeight(), getRandomWidthOrHeight());
+ 	}
+	
 
 	public JpgFile(int width, int height) {
 		super(EXTENSION, getContentBytes(width, height));
 	}
 
-	public JpgFile() {
-		this(getRandomWidthOrHeight(), getRandomWidthOrHeight());
-
-	}
-
 	private static int getRandomWidthOrHeight() {
 		return new Random().nextInt(1024);
 	}
-
-	private static byte[] getContentBytes(int width, int height) {
+	
+ 	private static byte[] getContentBytes(int width, int height) {
 		if (width <= 0 || height <= 0)
 			throw new IllegalArgumentException(String.format("width[%d] or height[%d] < 0 "
 					+ System.getProperty("line.seperator"), width, height));
 
 		BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
 		Graphics createGraphics = bufferedImage.getGraphics();
-		createGraphics.drawString(FileUtil.newContent(), 0, height / 2);
+		createGraphics.drawString(FileUtil.getRandomStringContent(), 0, height / 2);
 
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(byteArrayOutputStream);
@@ -45,14 +47,12 @@ public class JpgFile extends RandomFile {
 			createJPEGEncoder.encode(bufferedImage);
 			return byteArrayOutputStream.toByteArray();
 		} catch (ImageFormatException e1) {
-			e1.printStackTrace();
-			throw new RandomFileException(
+ 			throw new RandomFileException(
 					"image format exception when generating image file content", e1);
 		} catch (IOException e2) {
-			e2.printStackTrace();
-			throw new RandomFileException("io exception when generating image file content", e2);
+ 			throw new RandomFileException("io exception when generating image file content", e2);
 		} finally {
-			FileUtil.closeStream(bufferedOutputStream);
+			IOUtils.closeQuietly(bufferedOutputStream);
 		}
 
 	}
