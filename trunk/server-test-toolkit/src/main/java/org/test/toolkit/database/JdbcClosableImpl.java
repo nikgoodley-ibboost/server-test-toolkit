37d615714455382e83cc47d9c3e9921fdf727549
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2012-08-03 06:25:21 +0000

    add JdbcClosableImpl

---
+++ trunk/server-test-toolkit/src/main/java/org/test/toolkit/database/JdbcClosableImpl.java
@@ -0,0 +1,33 @@
+package org.test.toolkit.database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.test.toolkit.database.DbUtil.DbCloseUtil;
+
+/**
+ * @author fu.jian
+ * @date Aug 3, 2012
+ */
+public class JdbcClosableImpl implements JdbcClosable {
+
+	public JdbcClosableImpl() {
+	}
+
+	@Override
+	public void closeConnection(Connection connection) {
+		DbCloseUtil.closeConnection(connection);
+	}
+
+	@Override
+	public void closeResultSet(ResultSet resultSet) {
+		DbCloseUtil.closeResultSet(resultSet);
+	}
+
+	@Override
+	public void closeStatement(Statement statement) {
+		DbCloseUtil.closeStatement(statement);
+	}
+
+}
