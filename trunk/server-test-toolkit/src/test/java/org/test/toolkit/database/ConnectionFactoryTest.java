c2d496813489141cc19b8634a9cf8a8055449437
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2012-08-04 03:50:11 +0000

    add default getConnnection

---
+++ trunk/server-test-toolkit/src/test/java/org/test/toolkit/database/ConnectionFactoryTest.java
@@ -0,0 +1,13 @@
+package org.test.toolkit.database;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConnectionFactoryTest {
+
+	public static void main(String[] args) {
+		ConnectionFactory.getConnection();
+	}
+ 
+}
