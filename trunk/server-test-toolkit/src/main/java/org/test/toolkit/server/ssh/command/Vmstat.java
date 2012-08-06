61b0cfd55a29762bd4871a048562c091dba934da
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2012-08-06 03:35:40 +0000

    add performance data

--- trunk/server-test-toolkit/src/main/java/org/test/toolkit/server/ssh/command/Vmstat.java
+++ trunk/server-test-toolkit/src/main/java/org/test/toolkit/server/ssh/command/Vmstat.java
@@ -1,5 +1,9 @@
 package org.test.toolkit.server.ssh.command;
 
+import java.util.HashMap;
+
+import org.test.toolkit.server.ssh.performance.PerformanceData;
+
 /**
  * @author fu.jian
  * @date Jul 26, 2012
@@ -14,6 +18,29 @@ public class Vmstat extends Command {
 
 	public static Vmstat newInstance() {
 		return new Vmstat(VMSTAT_COMMAND_FORMAT);
+	}
+
+	public static PerformanceData parseString(String str) {
+		int startIndexForKeys = str.indexOf(" r");
+		int endIndexForKeys = str.indexOf(" st") + 3;
+		String[] keys = subStrAndSplitWithSpace(str, startIndexForKeys, endIndexForKeys);
+
+		int startIndexForValues = endIndexForKeys;
+		int endIndexForValues = str.length();
+		String[] values = subStrAndSplitWithSpace(str, startIndexForValues, endIndexForValues);
+
+		HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+		for (int i = 0; i < keys.length; i++) {
+			hashMap.put(keys[i], Integer.valueOf(values[i]));
+		}
+
+		return PerformanceData.fromMap(hashMap);
+	}
+
+	private static String[] subStrAndSplitWithSpace(String str, int startIndex, int endIndex) {
+		String rawKeys = str.substring(startIndex, endIndex);
+		String[] keys = rawKeys.trim().split("(\\s)+");
+		return keys;
 	}
 
 }
