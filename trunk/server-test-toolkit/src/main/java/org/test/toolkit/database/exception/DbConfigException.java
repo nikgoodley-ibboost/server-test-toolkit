7eb18e2599c94dd311ebc24cf8e6d1aec642054f
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2012-08-06 00:47:43 +0000

    remove constructor for exception

--- trunk/server-test-toolkit/src/main/java/org/test/toolkit/database/exception/DbConfigException.java
+++ trunk/server-test-toolkit/src/main/java/org/test/toolkit/database/exception/DbConfigException.java
@@ -18,10 +18,5 @@ public class DbConfigException extends DbException {
 	public DbConfigException(String message, Throwable cause) {
 		super(message, cause);
 	}
-
-	public DbConfigException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
+  
 }
