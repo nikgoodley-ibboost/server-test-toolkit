f5fb6053f5652b21323d02f00a10ed7ad9951bfd
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2012-08-06 13:59:36 +0000

    add xmlUtil

---
+++ trunk/server-test-toolkit/src/main/java/org/test/toolkit/util/XmlUtil.java
@@ -0,0 +1,14 @@
+package org.test.toolkit.util;
+
+import org.dom4j.Document;
+
+public final class XmlUtil {
+
+	private XmlUtil() {
+	}
+
+	public static Document getDocument(String xmlPath) {
+		return null;
+	}
+
+}
