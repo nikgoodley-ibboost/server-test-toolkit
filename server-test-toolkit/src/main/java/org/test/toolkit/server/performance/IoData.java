b4a901a68e7c8ee6cbfc676ed523c5e8247e748b
fujian1115 <fujian1115@gmail.com>
2012-08-06 07:24:14 +0000

    add IoData

---
+++ server-test-toolkit/src/main/java/org/test/toolkit/server/performance/IoData.java
@@ -0,0 +1,45 @@
+package org.test.toolkit.server.ssh.performance;
+
+public class IoData extends Component {
+
+	private static final String TYPE = "IO";
+
+	private int bi;
+	private int bo;
+
+	public IoData(int bi, int bo) {
+		super(TYPE);
+		this.bi = bi;
+		this.bo = bo;
+	}
+
+	public int getBi() {
+		return bi;
+	}
+
+	public void setBi(int bi) {
+		this.bi = bi;
+	}
+
+	public int getBo() {
+		return bo;
+	}
+
+	public void setBo(int bo) {
+		this.bo = bo;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("IoData [type=");
+		builder.append(type);
+		builder.append(", bi=");
+		builder.append(bi);
+		builder.append(", bo=");
+		builder.append(bo);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
