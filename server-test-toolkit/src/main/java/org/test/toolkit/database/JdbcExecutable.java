bd864fd258a5fc5fbc99ecfc58b324f26edd0979
fujian1115 <fujian1115@gmail.com>
2012-08-09 05:23:45 +0000

    add two methods

--- server-test-toolkit/src/main/java/org/test/toolkit/database/JdbcExecutable.java
+++ server-test-toolkit/src/main/java/org/test/toolkit/database/JdbcExecutable.java
@@ -1,15 +1,14 @@
 package org.test.toolkit.database;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import org.test.toolkit.database.resultsethandle.ResultSetHandle;
 
 /**
  * @author fu.jian
  * @date Aug 3, 2012
  */
 public interface JdbcExecutable {
-
-	ResultSet query(String sql);
 
 	boolean execute(String sql);
 
@@ -19,8 +18,10 @@ public interface JdbcExecutable {
 
 	int update(String sql, Object[] params) throws SQLException;
 
-	ResultSet query(String sql, Object[] params) throws SQLException;
-
 	boolean execute(String sql, Object[] params);
+
+	<T> T query(String sql, Object[] params, ResultSetHandle<T> resultHandle);
+
+	<T> T query(String sql, ResultSetHandle<T> resultHandle);
 
 }
