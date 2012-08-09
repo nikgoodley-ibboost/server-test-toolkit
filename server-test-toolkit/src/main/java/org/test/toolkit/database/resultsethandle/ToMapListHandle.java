7d39765946c3d5aca4026862f034793dd384afec
fujian1115 <fujian1115@gmail.com>
2012-08-09 05:26:57 +0000

    add ToMapListHandle

---
+++ server-test-toolkit/src/main/java/org/test/toolkit/database/resultsethandle/ToMapListHandle.java
@@ -0,0 +1,36 @@
+package org.test.toolkit.database.resultsethandle;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.test.toolkit.database.exception.DbExecuteException;
+
+public class ToMapListHandle implements ResultSetHandle<List<HashMap<String, ?>>> {
+
+	@Override
+	public List<HashMap<String, ?>> handle(ResultSet resultSet) {
+		List<HashMap<String, ?>> list = new ArrayList<HashMap<String, ?>>();
+		try {
+			while (resultSet.next()) {
+				HashMap<String, Object> hashMap = new HashMap<String, Object>();
+				ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+				int columnCount = resultSetMetaData.getColumnCount();
+				for (int index = 1; index < columnCount + 1; index++) {
+					String key = resultSetMetaData.getColumnName(index);
+					Object value = resultSet.getObject(index);
+					hashMap.put(key, value);
+				}
+
+				list.add(hashMap);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbExecuteException(e.getMessage(), e);
+		}
+	}
+
+}
