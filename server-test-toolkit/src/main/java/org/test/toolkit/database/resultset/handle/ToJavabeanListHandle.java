4c14fb6945d68c3433288cad785cf678a23d1ed8
fujian1115 <fujian1115@gmail.com>
2012-08-13 02:52:03 +0000

    add debug log and fix one bug for culumn index

--- server-test-toolkit/src/main/java/org/test/toolkit/database/resultset/handle/ToJavabeanListHandle.java
+++ server-test-toolkit/src/main/java/org/test/toolkit/database/resultset/handle/ToJavabeanListHandle.java
@@ -6,9 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.log4j.Logger;
 import org.test.toolkit.database.exception.DbExecuteException;
 
 public class ToJavabeanListHandle<T> implements ResultSetHandle<List<T>> {
+
+	private static final Logger LOGGER = Logger.getLogger(ToJavabeanListHandle.class);
 
 	private Class<T> javabeanClass;
 
@@ -18,21 +21,30 @@ public class ToJavabeanListHandle<T> implements ResultSetHandle<List<T>> {
 
 	@Override
 	public List<T> handle(ResultSet resultSet) {
+		LOGGER.info("[DB][ToJavabeanListHandle][BEGIN]");
 		List<T> list = new ArrayList<T>();
 		try {
 			while (resultSet.next()) {
 				T entity = javabeanClass.newInstance();
 				ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
 				int columnCount = resultSetMetaData.getColumnCount();
-				for (int index = 0; index < columnCount; index++) {
+				for (int index = 1; index <= columnCount; index++) {
 					String key = resultSetMetaData.getColumnName(index);
 					Object value = resultSet.getObject(index);
+					System.out.println(value.getClass());
+					if (LOGGER.isDebugEnabled()) {
+						String msg = String.format("[DB][Column][Key:Value][%s:%s]", key, value);
+						LOGGER.debug(msg);
+					}
+
 					BeanUtils.copyProperty(entity, key, value);
 				}
 				list.add(entity);
 			}
+			LOGGER.info("[DB][ToJavabeanListHandle][END][SUCCESS]");
 			return list;
 		} catch (Exception e) {
+			LOGGER.error("[DB][ToJavabeanListHandle][END][FAIL] " + e.getMessage(), e);
 			throw new DbExecuteException(e.getMessage(), e);
 		}
 	}
