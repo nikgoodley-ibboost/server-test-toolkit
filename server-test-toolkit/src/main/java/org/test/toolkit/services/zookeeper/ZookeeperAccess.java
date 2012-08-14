6ce222693bf98538cd955bb695326a82e3b9b835
fujian1115 <fujian1115@gmail.com>
2012-08-14 07:27:34 +0000

    add ZookeeperAccess

---
+++ server-test-toolkit/src/main/java/org/test/toolkit/services/zookeeper/ZookeeperAccess.java
@@ -0,0 +1,45 @@
+package org.test.toolkit.services.zookeeper;
+
+import java.util.List;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.data.Stat;
+
+public interface ZookeeperAccess {
+
+	String createSequenceNode(String parentPath, String path, byte[] data) throws KeeperException,
+			InterruptedException;
+
+	String createEphemeralNode(String path, byte[] data) throws KeeperException, InterruptedException;
+
+	String create(String path, byte data[]) throws KeeperException, InterruptedException;
+
+	List<String> getChildren(String path, boolean watch) throws KeeperException, InterruptedException;
+
+	List<String> getChildren(String path, Watcher watch) throws KeeperException, InterruptedException;
+
+	long getSessionId();
+
+	boolean isAvalable();
+
+	Stat exists(String path, Watcher watcher) throws KeeperException, InterruptedException;
+
+	Stat exists(String path, boolean watcher) throws KeeperException, InterruptedException;
+
+	void delete(String path, int version) throws InterruptedException, KeeperException;
+
+	Stat setData(String path, byte data[], int version) throws KeeperException, InterruptedException;
+
+	void ensurePathExists(String path) throws KeeperException, InterruptedException;
+
+	byte[] getData(String siblePath, boolean watch, Stat stat) throws KeeperException, InterruptedException;
+
+	byte[] getData(String path, Watcher watcher, Stat stat) throws KeeperException, InterruptedException;
+
+	String getConnectStr();
+
+	void process(WatchedEvent event);
+
+}
