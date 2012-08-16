6e700c90052dc7524f5edc8345bf05a92a727476
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2012-08-16 02:58:40 +0000

    remove

--- trunk/server-test-toolkit/src/main/java/org/test/toolkit/services/exception/ServiceConfigException.java
+++ trunk/server-test-toolkit/src/main/java/org/test/toolkit/services/exception/ServiceConfigException.java
@@ -19,9 +19,4 @@ public class ServiceConfigException extends ServiceException {
 		super(arg0, arg1);
  	}
 
-	public ServiceConfigException(String arg0, Throwable arg1, boolean arg2,
-			boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
- 	}
-
 }
