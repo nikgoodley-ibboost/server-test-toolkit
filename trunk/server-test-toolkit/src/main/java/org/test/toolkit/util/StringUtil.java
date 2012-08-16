a254734b1b243b05e16e159ff91d42249b55b821
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2012-08-16 03:19:52 +0000

    add java doc

--- trunk/server-test-toolkit/src/main/java/org/test/toolkit/util/StringUtil.java
+++ trunk/server-test-toolkit/src/main/java/org/test/toolkit/util/StringUtil.java
@@ -2,9 +2,15 @@ package org.test.toolkit.util;
 
 import org.test.toolkit.constants.MarkConstants;
 
-
 public final class StringUtil {
 
+	/**
+	 * to format such as "stringOne stringTwo stringThird" which takes the space
+	 * as the spliter.
+	 * 
+	 * @param strings
+	 * @return
+	 */
 	public static String concatWithSpace(String... strings) {
 		ValidationUtil.checkNull(strings);
 		StringBuffer stringBuffer = new StringBuffer();
@@ -13,16 +19,22 @@ public final class StringUtil {
 			stringBuffer.append(MarkConstants.SPACE);
 		}
 
-  		return stringBuffer.toString().trim();
+		return stringBuffer.toString().trim();
 	}
 
+	/**
+	 * concat all strings directly.
+	 * 
+	 * @param strings
+	 * @return
+	 */
 	public static String concatDirectly(String... strings) {
- 		StringBuffer stringBuffer = new StringBuffer();
+		StringBuffer stringBuffer = new StringBuffer();
 		for (String string : strings) {
 			stringBuffer.append(string);
- 		}
+		}
 
-   		return stringBuffer.toString().trim();
+		return stringBuffer.toString().trim();
 	}
 
 	private StringUtil() {
