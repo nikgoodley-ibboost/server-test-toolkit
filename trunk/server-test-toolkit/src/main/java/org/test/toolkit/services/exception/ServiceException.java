9cd3d473fdf348e2d8a2e530d86ffe0a7805dfc8
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2012-08-16 02:58:18 +0000

    remove unuserd method

--- trunk/server-test-toolkit/src/main/java/org/test/toolkit/services/exception/ServiceException.java
+++ trunk/server-test-toolkit/src/main/java/org/test/toolkit/services/exception/ServiceException.java
@@ -19,9 +19,4 @@ public class ServiceException extends RuntimeException {
 		super(arg0, arg1);
  	}
 
-	public ServiceException(String arg0, Throwable arg1, boolean arg2,
-			boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
- 	}
-
 }
