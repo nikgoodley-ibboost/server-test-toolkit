980640d6d1d3f7d68bbcc3f936d7a68cdf4760bc
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2012-09-05 01:58:17 +0000

    remove the inet support

--- trunk/server-test-toolkit/src/main/java/org/test/toolkit/database/config/OracleConfig.java
+++ trunk/server-test-toolkit/src/main/java/org/test/toolkit/database/config/OracleConfig.java
@@ -6,18 +6,10 @@ public class OracleConfig extends ExplicitDbConfig {
 	 * {@value}
 	 */
 	private static final String JDBC_DRIVER_FOR_ORACLE = "oracle.jdbc.driver.OracleDriver";
-	private static final String JDBC_DRIVER_FOR_INET = "com.inet.ora.OraDriver";
 
-	public static OracleConfig getInstanceForOracle(String url, String username, String password) {
-		return new OracleConfig(JDBC_DRIVER_FOR_ORACLE, url, username, password);
-	}
 
-	public static OracleConfig getInstanceForInet(String url, String username, String password) {
-		return new OracleConfig(JDBC_DRIVER_FOR_INET, url, username, password);
-	}
-
-	private OracleConfig(String driverClass, String url, String username, String password) {
-		super(driverClass, url, username, password);
+	public OracleConfig(String url, String username, String password) {
+		super(JDBC_DRIVER_FOR_ORACLE, url, username, password);
 	}
 
 }
