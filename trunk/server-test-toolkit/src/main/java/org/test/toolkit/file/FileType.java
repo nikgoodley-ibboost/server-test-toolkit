0afc97e887019885cc8debbee7230e421433a490
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2012-11-08 08:10:01 +0000

    change letter to upper

--- trunk/server-test-toolkit/src/main/java/org/test/toolkit/file/FileType.java
+++ trunk/server-test-toolkit/src/main/java/org/test/toolkit/file/FileType.java
@@ -1,12 +1,12 @@
 package org.test.toolkit.file;
 
 enum FileCategory {
-	Image, DOCUMENT, PLAIN_TXT, OTHER;
+	IMAGE, DOCUMENT, PLAIN_TXT, OTHER;
 }
 
 public enum FileType {
 
-	DOC(FileCategory.DOCUMENT), JPG(FileCategory.Image), GIF(FileCategory.Image), TXT(FileCategory.PLAIN_TXT), PDF(
+	DOC(FileCategory.DOCUMENT), JPG(FileCategory.IMAGE), GIF(FileCategory.IMAGE), TXT(FileCategory.PLAIN_TXT), PDF(
 			FileCategory.OTHER);
 
 	private FileCategory type;
