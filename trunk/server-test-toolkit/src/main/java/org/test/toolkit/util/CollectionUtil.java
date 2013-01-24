6781f6da8295a05d2def8ca0e89251d834995a54
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2013-01-24 02:56:43 +0000

    add mergeValues method

--- trunk/server-test-toolkit/src/main/java/org/test/toolkit/util/CollectionUtil.java
+++ trunk/server-test-toolkit/src/main/java/org/test/toolkit/util/CollectionUtil.java
@@ -2,11 +2,12 @@ package org.test.toolkit.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 /**
- * @author fu.jian
- * date Jul 26, 2012
+ * @author fu.jian date Jul 26, 2012
  */
 public final class CollectionUtil {
 
@@ -15,6 +16,22 @@ public final class CollectionUtil {
 		allTs.add(atLeastOneT);
 
 		return allTs;
+	}
+
+	/**
+	 * merge map's value to string
+	 * @param map
+	 * @return
+	 */
+	public static <T> String mergeValues(Map<T, String> map) {
+		Collection<String> values = map.values();
+
+		StringBuffer stringBuffer = new StringBuffer();
+		for (String value : values) {
+			stringBuffer.append(value);
+		}
+
+		return stringBuffer.toString();
 	}
 
 	private CollectionUtil() {
