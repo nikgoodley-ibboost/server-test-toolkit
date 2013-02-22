b9224ee0c4adb781196a60c941120c67f67458c2
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2013-02-22 03:18:41 +0000

    add sleep(String timeStr) to improve sleepUtil. such as sleep("10s")

--- trunk/server-test-toolkit/src/main/java/org/test/toolkit/util/SleepUtil.java
+++ trunk/server-test-toolkit/src/main/java/org/test/toolkit/util/SleepUtil.java
@@ -1,22 +1,55 @@
 package org.test.toolkit.util;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 
 public class SleepUtil {
 
+	public final static Map<String, TimeUnit> CONVERT_MAP_FOR_TIMEUNIT = new HashMap<String, TimeUnit>();
 	private final static Logger LOGGER = Logger.getLogger(SleepUtil.class);
+
+	static {
+		CONVERT_MAP_FOR_TIMEUNIT.put("d", TimeUnit.DAYS);
+		CONVERT_MAP_FOR_TIMEUNIT.put("h", TimeUnit.HOURS);
+		CONVERT_MAP_FOR_TIMEUNIT.put("m", TimeUnit.MINUTES);
+		CONVERT_MAP_FOR_TIMEUNIT.put("s", TimeUnit.SECONDS);
+		CONVERT_MAP_FOR_TIMEUNIT.put("ms", TimeUnit.MILLISECONDS);
+		CONVERT_MAP_FOR_TIMEUNIT.put("us", TimeUnit.MICROSECONDS);
+		CONVERT_MAP_FOR_TIMEUNIT.put("ns", TimeUnit.NANOSECONDS);
+	}
 
 	private SleepUtil() {
 	}
 
 	public static void sleep(TimeUnit timeUnit, long time) {
 		try {
+			String lowerCaseTimeUnitStr = timeUnit.toString().toLowerCase();
+			LOGGER.info(String.format("[util][time][sleep][begin][%d][%s]",time,lowerCaseTimeUnitStr));
 			timeUnit.sleep(time);
+			LOGGER.info(String.format("[util][time][sleep][end][%d][%s]",time,lowerCaseTimeUnitStr));
 		} catch (InterruptedException e) {
 			LOGGER.warn("stop waiting for:" + e.getMessage());
 		}
 	}
 
-}
+	/**
+	 * such as 10s or 10S, it hint 10 seconds
+	 *
+	 * @param time
+	 */
+	public static void sleep(String timeStr) {
+		ValidationUtil.checkString(timeStr);
+
+		String trimedTimeStr = timeStr.trim();
+		String timeUnitStr = trimedTimeStr.replaceAll("\\d", "");
+ 		String timeNumberStr = trimedTimeStr.replaceAll("\\D", "");
+
+		LOGGER.info(String.format("[util][time][sleep][convert][%s][%s]",timeNumberStr,timeUnitStr));
+
+		sleep(CONVERT_MAP_FOR_TIMEUNIT.get(timeUnitStr.toLowerCase()), Long.valueOf(timeNumberStr));
+ 	}
+
+ }
