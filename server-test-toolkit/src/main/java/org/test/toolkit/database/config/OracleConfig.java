5b62b11d69f342d29dc59c4342a5422f2f563ff8
fujian1115 <fujian1115@gmail.com>
2013-05-06 05:09:26 +0000

    add java doc for oracle db url's example

--- server-test-toolkit/src/main/java/org/test/toolkit/database/config/OracleConfig.java
+++ server-test-toolkit/src/main/java/org/test/toolkit/database/config/OracleConfig.java
@@ -8,8 +8,14 @@ public class OracleConfig extends ExplicitDbConfig {
 	private static final String JDBC_DRIVER_FOR_ORACLE = "oracle.jdbc.driver.OracleDriver";
 
 
+	/**
+	 * @param url such as:jdbc:oracle:thin:@10.1.1.2:1521:shdb
+	 * @param username
+	 * @param password
+	 */
 	public OracleConfig(String url, String username, String password) {
 		super(JDBC_DRIVER_FOR_ORACLE, url, username, password);
 	}
 
 }
+
