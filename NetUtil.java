16597026d1c3f09d2036e0e357cf814d352b0210
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2013-05-22 07:28:15 +0000

    add NetUtil class for get Local ip, etc

---
+++ NetUtil.java
@@ -0,0 +1,20 @@
+package org.test.toolkit.util;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+public class NetUtil {
+
+	private NetUtil() {
+		// no instance;
+	}
+
+	public static String getLocalIp() {
+		try {
+			return InetAddress.getLocalHost().getHostAddress().toString();
+		} catch (UnknownHostException e) {
+			throw new UtilException(e.getMessage(), e);
+		}
+	}
+}
