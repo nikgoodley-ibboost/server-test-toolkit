cc3e4e8f69978c52c2709333bac0a83d637e4948
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2014-03-18 05:50:15 +0000

    add javadoc for mysql db url

--- trunk/server-test-toolkit/src/main/java/com/googlecode/test/toolkit/database/config/MysqlConfig.java
+++ trunk/server-test-toolkit/src/main/java/com/googlecode/test/toolkit/database/config/MysqlConfig.java
@@ -7,6 +7,11 @@ public class MysqlConfig extends ExplicitDbConfig {
 	 */
 	private static final String JDBC_DRIVER_FOR_MYSQL = "com.mysql.jdbc.Driver";
 
+	/**
+	 * @param url  jdbc:mysql://localhost:3306:test
+	 * @param username
+	 * @param password
+	 */
 	public MysqlConfig(String url, String username, String password) {
 		super(JDBC_DRIVER_FOR_MYSQL, url, username, password);
 	}
