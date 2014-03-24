78c3c44441c5f3e224ac1a943435d6b1a1589306
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2014-03-24 09:29:47 +0000

    add dns name to ip method

--- trunk/server-test-toolkit/src/main/java/com/googlecode/test/toolkit/util/NetUtil.java
+++ trunk/server-test-toolkit/src/main/java/com/googlecode/test/toolkit/util/NetUtil.java
@@ -3,18 +3,26 @@ package com.googlecode.test.toolkit.util;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-
 public class NetUtil {
 
-	private NetUtil() {
-		// no instance;
-	}
+    private NetUtil() {
+        // no instance;
+    }
 
-	public static String getLocalIp() {
-		try {
-			return InetAddress.getLocalHost().getHostAddress().toString();
-		} catch (UnknownHostException e) {
-			throw new UtilException(e.getMessage(), e);
-		}
-	}
+    public static String getLocalIp() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress().toString();
+        } catch (UnknownHostException e) {
+            throw new UtilException(e.getMessage(), e);
+        }
+    }
+
+    public static String toIp(String dnsName) {
+        try {
+            return InetAddress.getByName(dnsName).getHostAddress();
+        } catch (UnknownHostException e) {
+            throw new UtilException(e.getMessage(), e);
+        }
+    }
+
 }
