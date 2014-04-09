e31937a0762ebc5f25511440181c0827fc77f422
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2014-04-09 03:48:49 +0000

    fix the result handle for retry listener

--- trunk/server-test-toolkit/src/main/java/com/googlecode/test/toolkit/casewrapper/testng/TestRetryListener.java
+++ trunk/server-test-toolkit/src/main/java/com/googlecode/test/toolkit/casewrapper/testng/TestRetryListener.java
@@ -2,6 +2,14 @@ package com.googlecode.test.toolkit.casewrapper.testng;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.testng.IAnnotationTransformer;
 import org.testng.IRetryAnalyzer;
@@ -10,7 +18,7 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.annotations.ITestAnnotation;
 
-public class TestRetryListener  implements IAnnotationTransformer,ITestListener  {
+public class TestRetryListener implements IAnnotationTransformer, ITestListener {
 
 	@SuppressWarnings("rawtypes")
 	@Override
@@ -24,44 +32,80 @@ public class TestRetryListener  implements IAnnotationTransformer,ITestListener 
 
 	@Override
 	public void onTestStart(ITestResult paramITestResult) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult paramITestResult) {
-		// TODO Auto-generated method stub
-		
+ 
 	}
 
 	@Override
 	public void onTestFailure(ITestResult paramITestResult) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult paramITestResult) {
-		// TODO Auto-generated method stub
-		
+ 
 	}
 
 	@Override
 	public void onTestFailedButWithinSuccessPercentage(
 			ITestResult paramITestResult) {
-		// TODO Auto-generated method stub
-		
+ 
 	}
 
 	@Override
 	public void onStart(ITestContext paramITestContext) {
-		// TODO Auto-generated method stub
-		
+ 
 	}
 
 	@Override
 	public void onFinish(ITestContext paramITestContext) {
-		// TODO Auto-generated method stub
-		
+		Set<ITestResult> allResults = new HashSet<ITestResult>();
+		allResults.addAll(paramITestContext.getPassedTests().getAllResults());
+		allResults.addAll(paramITestContext.getFailedTests().getAllResults());
+		allResults.addAll(paramITestContext.getSkippedTests().getAllResults());
+
+		Map<Method, ArrayList<ITestResult>> allITestResultMap = new HashMap<Method, ArrayList<ITestResult>>();
+		for (ITestResult iTestResult : allResults) {
+			@SuppressWarnings("deprecation")
+			Method method = iTestResult.getMethod().getMethod();
+			if (allITestResultMap.get(method) == null) {
+				allITestResultMap.put(method, new ArrayList<ITestResult>());
+				allITestResultMap.get(method).add(iTestResult);
+
+			} else {
+				allITestResultMap.get(method).add(iTestResult);
+			}
+		}
+
+		Collection<ArrayList<ITestResult>> resultMapValues = allITestResultMap
+				.values();
+		ArrayList<ITestResult> resultArrayList = new ArrayList<ITestResult>();
+
+		for (ArrayList<ITestResult> arrayList : resultMapValues) {
+			if (arrayList.size() > 1) {
+				Comparator<ITestResult> c = new Comparator<ITestResult>() {
+
+					@Override
+					public int compare(ITestResult o1, ITestResult o2) {
+						return (int) (o1.getStartMillis() - o2.getStartMillis());
+					}
+				};
+				Collections.sort(arrayList, c);
+				arrayList.remove(arrayList.size() - 1);
+				resultArrayList.addAll(arrayList);
+			}
+
+		}
+
+		for (ITestResult iTestResult : resultArrayList) {
+			paramITestContext.getPassedTests().removeResult(iTestResult);
+			paramITestContext.getFailedTests().removeResult(iTestResult);
+			paramITestContext.getSkippedTests().removeResult(iTestResult);
+		}
+
 	}
 }
