8a35ddd1572587f523fcded823594c3f428aba35
fujian1115@gmail.com <fujian1115@gmail.com@813a096d-ad59-38b0-1fd0-3b285362ec8d>
2014-04-23 09:45:28 +0000

    fix bug: dead loop

--- trunk/server-test-toolkit/src/main/java/com/googlecode/test/toolkit/server/common/util/JSchUtil.java
+++ trunk/server-test-toolkit/src/main/java/com/googlecode/test/toolkit/server/common/util/JSchUtil.java
@@ -43,7 +43,7 @@ public final class JSchUtil {
 	public static class JSchSessionUtil {
 
  		public static SessionWrapper getSessionWrapper(ServerUser serverUser, int maxChannelNumber) {
- 			return getSessionWrapper(serverUser,maxChannelNumber);
+ 			return new SessionWrapper(getSession(serverUser),serverUser,maxChannelNumber);
 		}
 
 		public static SessionWrapper getSessionWrapper(ServerUser serverUser) {
